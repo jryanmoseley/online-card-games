80e34bba0fabf432bdf1b23672fba759ac2b4ac0
Jonathan Moseley <jonathanmoseley@Jonathans-MacBook-Pro.local>
2022-09-17 01:15:33 -0400

    added card images and updated css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
 
+const cardImages = [
+  { "src": "img/arsenal.png" },
+  { "src": "img/aston_villa.png" },
+  { "src": "img/bournemouth.png" },
+  { "src": "img/chelsea.png" },
+  { "src": "img/crystal_palace.png" },
+  { "src": "img/everton.png" },
+  { "src": "img/leicester_city.png" },
+  { "src": "img/liverpool.png" },
+  { "src": "img/manchester_city.png" },
+  { "src": "img/manchester_united.png" },
+  { "src": "img/newcastle_united.png" },
+  { "src": "img/norwich_city.png" },
+  { "src": "img/southampton.png" },
+  { "src": "img/stoke_city.png" },
+  { "src": "img/sunderland.png" },
+  { "src": "img/swansea_city.png" },
+  { "src": "img/tottenham_hotspur.png" },
+  { "src": "img/watford.png" },
+  { "src": "img/westbrom.png" },
+  { "src": "img/westham.png" },
+];
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <h1>
           Online Card Games
         </h1>
-      </header>
+        <button>New Memory Game</button>
     </div>
   );
 }
