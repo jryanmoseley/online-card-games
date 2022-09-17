a80a528ba9ea51a20094ea1a520e7559cf38f986
Jonathan Moseley <jonathanmoseley@Jonathans-MacBook-Pro.local>
2022-09-17 10:09:38 -0400

    added new memory game function

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 const cardImages = [
-  { "src": "img/arsenal.png" },
-  { "src": "img/aston_villa.png" },
-  { "src": "img/bournemouth.png" },
-  { "src": "img/chelsea.png" },
-  { "src": "img/crystal_palace.png" },
-  { "src": "img/everton.png" },
-  { "src": "img/leicester_city.png" },
-  { "src": "img/liverpool.png" },
-  { "src": "img/manchester_city.png" },
-  { "src": "img/manchester_united.png" },
-  { "src": "img/newcastle_united.png" },
-  { "src": "img/norwich_city.png" },
-  { "src": "img/southampton.png" },
-  { "src": "img/stoke_city.png" },
-  { "src": "img/sunderland.png" },
-  { "src": "img/swansea_city.png" },
-  { "src": "img/tottenham_hotspur.png" },
-  { "src": "img/watford.png" },
-  { "src": "img/westbrom.png" },
-  { "src": "img/westham.png" },
+  { src: "img/arsenal.png" },
+  { src: "img/aston_villa.png" },
+  { src: "img/bournemouth.png" },
+  { src: "img/chelsea.png" },
+  { src: "img/crystal_palace.png" },
+  { src: "img/everton.png" },
+  { src: "img/leicester_city.png" },
+  { src: "img/liverpool.png" },
+  { src: "img/manchester_city.png" },
+  { src: "img/manchester_united.png" },
+  { src: "img/newcastle_united.png" },
+  { src: "img/norwich_city.png" },
+  { src: "img/southampton.png" },
+  { src: "img/stoke_city.png" },
+  { src: "img/sunderland.png" },
+  { src: "img/swansea_city.png" },
+  { src: "img/tottenham_hotspur.png" },
+  { src: "img/watford.png" },
+  { src: "img/westbrom.png" },
+  { src: "img/westham.png" },
 ];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const newMemoryGame = (numberOfCards) => {
+    const cardImagesSubset = [...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .splice(0, numberOfCards);
+
+    const shuffledCards = [...cardImagesSubset, ...cardImagesSubset]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
-        <h1>
-          Online Card Games
-        </h1>
-        <button>New Memory Game</button>
+      <h1>Online Card Games</h1>
+      <button onClick={() => newMemoryGame(6)}>New Memory Game</button>
     </div>
   );
 }
